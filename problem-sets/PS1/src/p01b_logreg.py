0b430c6c4e17952b6ec579c17ba263dc69b83a16
Vipul Pandey <54221581+vipulpandey12345@users.noreply.github.com>
2025-05-20 02:04:52 -0700

    started problems hw #1

--- problem-sets/PS1/src/p01b_logreg.py
+++ problem-sets/PS1/src/p01b_logreg.py
@@ -12,9 +12,14 @@ def main(train_path, eval_path, pred_path):
         eval_path: Path to CSV file containing dataset for evaluation.
         pred_path: Path to save predictions.
     """
-    x_train, y_train = util.load_dataset(train_path, add_intercept=True)
-
     # *** START CODE HERE ***
+    x_train, y_train = util.load_dataset(train_path, add_intercept=True)
+    x_eval, y_eval = util.load_dataset(eval_path, add_intercept=True)
+    model = LogisticRegression(eps=1e-5)
+    model.fit(x_train, y_train)
+    util.plot(x_train, y_train, model.theta, 'output/p01b_{}.png'.format(pred_path[-5]))
+    y_pred = model.predict(x_eval)
+    np.savetxt(pred_path, y_pred > 0.5, fmt='%d')
     # *** END CODE HERE ***
 
 
@@ -35,6 +40,19 @@ class LogisticRegression(LinearModel):
             y: Training example labels. Shape (m,).
         """
         # *** START CODE HERE ***
+        # Init theta
+        theta = np.zeros(x.shape[1])
+        m, n = x.shape
+        while True:
+            #save a copy of theta
+            theta_copy = np.copy(theta)
+            #compute hessian
+            h_x = 1 / (1 + np.exp(-x.dot(theta)))
+            H = (x.T * h_x * (1 - h_x)).dot(x) / m
+            #compute gradient
+            
+
+
         # *** END CODE HERE ***
 
     def predict(self, x):
